822fec8250858a782f140d714efe01c919792b7a
Elyott Ryan <elyott_ryan@hotmail.com>
2018-08-25 16:25:36 -0700

    Added new file for JokeBook component

---
+++ src/JokeBook.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class JokeBook extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="jokebook_container">
+
+      </div>
+    );
+  }
+}
+
+export default JokeBook;
