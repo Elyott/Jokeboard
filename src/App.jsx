e6a8afb8a1ffebba400e60cd6029086a1dc85432
Elyott Ryan <elyott_ryan@hotmail.com>
2018-08-25 15:46:56 -0700

    Import navbar file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import NavBar from './NavBar.jsx';
+
 class App extends Component {
 
   constructor(){
@@ -12,19 +14,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar">
-          <a href="/" className="navbar-brand">JOKERNAUT</a>
-          <div className="buttons">
-            <div className="home_button">
-              <a>Add Joke</a>
-            </div>
-            <div className="notebook_button">
-              <a>Notebook</a>
-            </div>
-          </div>
-        </nav>
-
-
+        <NavBar />
       </div>
     );
   }
