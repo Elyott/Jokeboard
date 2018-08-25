52a83b068912f89ed5dd5fe5fdbdd3ffc3545c52
Elyott Ryan <elyott_ryan@hotmail.com>
2018-08-25 16:26:03 -0700

    Imported JokeBook component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React, {Component} from 'react';
 
 import NavBar from './NavBar.jsx';
+import JokeBook from './JokeBook';
 
 class App extends Component {
-
   constructor(){
     super();
-
-
-
   }
 
   render() {
     return (
       <div>
         <NavBar />
+        <JokeBook />
       </div>
     );
   }
 }
+
 export default App;
