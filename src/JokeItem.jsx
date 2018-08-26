e3dad83d3c9934e8f947787436a383687fc6ce20
Elyott Ryan <elyott_ryan@hotmail.com>
2018-08-25 17:15:37 -0700

    Changed joke.jsx to JokeItem.jsx for clarification

---
+++ src/JokeItem.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class JokeItem extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="joke">
+        Joke Name
+      </div>
+    );
+  }
+}
+
+export default JokeItem;
