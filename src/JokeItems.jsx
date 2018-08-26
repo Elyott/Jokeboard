8f4ffff040da065a142e41ce4e6d9eedb052ef35
Elyott Ryan <elyott_ryan@hotmail.com>
2018-08-25 17:03:39 -0700

    Created JokeItem.jsx file and added jokes div

---
+++ src/JokeItems.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class JokeItems extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="jokes">
+        You Have No Jokes Yet
+      </div>
+    );
+  }
+}
+
+export default JokeItems;
