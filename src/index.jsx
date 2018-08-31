081694440e2702bb59b5c28dc117d0ab45841e60
Elyott Ryan <elyott_ryan@hotmail.com>
2018-08-31 16:08:19 -0700

    Created redux store and imported it in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -7,5 +7,11 @@ require('../styles/application.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+ReactDOM.render(
+<Provider store={configureStore()}>
+  <App />
+</Provider>,
+document.getElementById('react-root'));
