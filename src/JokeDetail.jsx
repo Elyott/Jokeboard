ba806d1ca9b9bee927657b3e90f305eb1dbd6841
Elyott Ryan <elyott_ryan@hotmail.com>
2018-09-01 13:24:52 -0700

    Displays more joke details

--- src/JokeDetail.jsx
+++ src/JokeDetail.jsx
@@ -4,9 +4,16 @@ import { connect } from 'react-redux';
 class JokeDetail extends Component {
   render() {
     return (
-      <div>
-        <div>Joke Details:</div>
-        {!this.props.joke && <div>hi</div> }
+      <div className="jokebook_container joke_detail">
+        {this.props.joke &&
+          <div>
+            <div>Joke Details:</div>
+            <div>Title: {this.props.joke.title}</div>
+            <div>Duration: {this.props.joke.duration}</div>
+            <div>Rating: {this.props.joke.rating}</div>
+            <div>Content: {this.props.joke.content}</div>
+          </div>
+        }
       </div>
     );
   }
