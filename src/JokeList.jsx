42723968fdbfec8cdd59e45b5a1ff26f89336608
Elyott Ryan <elyott_ryan@hotmail.com>
2018-08-31 18:08:25 -0700

    Passed down selectJoke function to JokeItem

--- src/JokeList.jsx
+++ src/JokeList.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import JokeItem from './JokeItem.jsx';
+import { selectJoke } from './actions/index'
+import { bindActionCreators } from 'redux';
 
 class JokeList extends Component {
   renderJokeList() {
     return this.props.jokes.map((joke) => {
-      return (<JokeItem key={joke.title} joke={joke} /> )
+      return (<JokeItem
+        key={joke.title}
+        selectJoke={this.props.selectJoke}
+        joke={joke} /> )
     })
   }
 
@@ -15,9 +20,14 @@ class JokeList extends Component {
       </div>;
   }
 }
+
 const mapStateToProps = state => {
   return {
     jokes: state.jokes
   }
 }
-export default connect(mapStateToProps)(JokeList);
+
+const mapDispatchToProps = dispatch => {
+  return {selectJoke: selectJoke(dispatch)};
+}
+export default connect(mapStateToProps, mapDispatchToProps)(JokeList);
