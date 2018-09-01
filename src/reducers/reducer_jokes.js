b12259f5aa27445b6cac203514304386dd9ee37b
Elyott Ryan <elyott_ryan@hotmail.com>
2018-09-01 13:24:28 -0700

    Added more detail to joke 1

--- src/reducers/reducer_jokes.js
+++ src/reducers/reducer_jokes.js
@@ -1,6 +1,6 @@
 export default function() {
   return [
-    {title: 'Joke 1'},
+    {title: 'Two Dogs', duration: '60 sec', rating: 'B', content: 'Two dogs walk into a bar, one pees all over the place the other just laughs.' },
     {title: 'Joke 2'},
     {title: 'Joke 3'},
     {title: 'Joke 4'},
