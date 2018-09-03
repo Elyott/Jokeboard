acb0dd2206873de9ea257aab28ebada2e3d394f5
Elyott Ryan <elyott_ryan@hotmail.com>
2018-09-03 13:29:59 -0700

    Titles now show up without joke details

--- src/JokeDetail.jsx
+++ src/JokeDetail.jsx
@@ -4,16 +4,12 @@ import { connect } from 'react-redux';
 class JokeDetail extends Component {
   render() {
     return (
-      <div className="jokebook_container joke_detail">
-        {this.props.joke &&
-          <div>
-            <div>Joke Details:</div>
-            <div>Title: {this.props.joke.title}</div>
-            <div>Duration: {this.props.joke.duration}</div>
-            <div>Rating: {this.props.joke.rating}</div>
-            <div>Content: {this.props.joke.content}</div>
-          </div>
-        }
+      <div className='joke_detail'>
+        <div className='joke_detail_title' >Joke</div>
+        <div>Title: {this.props.joke && this.props.joke.title}</div>
+        <div>Duration: {this.props.joke && this.props.joke.duration}</div>
+        <div>Rating: {this.props.joke && this.props.joke.rating}</div>
+        <div>Content: {this.props.joke && this.props.joke.content}</div>
       </div>
     );
   }
