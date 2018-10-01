ed65eb7b075b656afa9c705c7f7b0335dc74d1a5
Elyott Ryan <elyott_ryan@hotmail.com>
2018-10-01 11:35:58 -0700

    Fetched data from express server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,44 @@ import React, {Component} from 'react';
 
 import NavBar from './NavBar.jsx';
 import JokeBook from './JokeBook.jsx';
+import "babel-polyfill";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
+
+    this.state = { response: "" };
   }
+
+  componentDidMount() {
+
+    fetch("http://localhost:5000/api/hello")
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        this.setState({ response: data.express });
+      });
+    // this.callApi()
+    //   .then(res => this.setState({ response: res.express }))
+    //   .catch(err => console.log(err));
+  }
+
+  // callApi = async () => {
+  //   const response = await fetch("http://localhost:5000/api/hello");
+  //   const body = await response.json();
+
+  //   if (response.status !== 200) throw Error(body.message);
+
+  //   return body;
+  // };
 
   render() {
     return (
       <div>
         <NavBar />
         <JokeBook />
+        <p className="App-intro">{this.state.response}</p>
       </div>
     );
   }
