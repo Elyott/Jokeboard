bf7066659e01569301fd0fa07d2a6d6b2a055a02
Elyott Ryan <elyott_ryan@hotmail.com>
2018-10-04 16:04:00 -0700

    Imported routes from routes folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const routes = require('./routes');
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -9,10 +10,8 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get('/api/hello', (req, res) => {
-  res.send({
-    express: 'Hello From Express'
-  });
-});
+//  Connect all our routes to our application
+app.use('/api', routes);
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
