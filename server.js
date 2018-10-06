b21eee0ae72d5d4e344fa966323125e2f2788e31
Elyott Ryan <elyott_ryan@hotmail.com>
2018-10-06 10:15:36 -0700

    added static path for react client

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const routes = require('./routes');
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
