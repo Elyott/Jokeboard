c3eb5b2451ae5a385fde3d455e3ec51e654ef70e
Elyott Ryan <elyott_ryan@hotmail.com>
2018-10-07 15:14:41 -0700

    Created a new client folder using create-react-app

--- server.js
+++ server.js
@@ -10,19 +10,23 @@ const pool = new Pool({
   ssl: true
 });
 
-app.use(express.static(path.join(__dirname, 'client/dist/')));
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+// if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+// }
+
 //  Connect all our routes to our application
 app.use('/api', routes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
