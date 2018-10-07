93bb6b235f8a7f1325146a0c4c9411d5bba840c6
Elyott Ryan <elyott_ryan@hotmail.com>
2018-10-07 15:38:51 -0700

    Removed logo, imported scss, added react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
+
+
+import NavBar from './NavBar';
+// import JokeBook from './JokeBook';
+import "babel-polyfill";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { response: [] };
+  }
+
+  componentDidMount() {
+
+    fetch("http://localhost:5000/api/jokes")
+      .then(results => {
+        console.log(results);
+        return results.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.setState({ response: data });
+      });
+    // this.callApi()
+    //   .then(res => this.setState({ response: res.express }))
+    //   .catch(err => console.log(err));
+  }
+
+  // callApi = async () => {
+  //   const response = await fetch("http://localhost:5000/api/hello");
+  //   const body = await response.json();
+
+  //   if (response.status !== 200) throw Error(body.message);
+
+  //   return body;
+  // };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <NavBar />
+        {/* <JokeBook /> */}
+        <div className="App-intro">{this.state.response.map((joke) => {return <div key={joke.id}>Name: {joke.name} Content: {joke.content} </div> })}</div>
       </div>
     );
   }
