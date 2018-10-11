b721ed591103426167c21624e50ef5090f2fd1af
Elyott Ryan <elyott_ryan@hotmail.com>
2018-10-11 16:15:02 -0700

    Cleaned up code

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,17 @@
 import React, { Component } from 'react';
 import './styles/App.scss';
 
-
 import NavBar from './NavBar';
 import JokeBook from './JokeBook';
 
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = { response: [] };
-  }
-
-  componentDidMount() {
-
-    fetch("http://localhost:5000/api/jokes")
-      .then(results => {
-        console.log(results);
-        return results.json();
-      })
-      .then(data => {
-        console.log(data);
-        this.setState({ response: data });
-      });
-    // this.callApi()
-    //   .then(res => this.setState({ response: res.express }))
-    //   .catch(err => console.log(err));
-  }
-
-  // callApi = async () => {
-  //   const response = await fetch("http://localhost:5000/api/hello");
-  //   const body = await response.json();
-
-  //   if (response.status !== 200) throw Error(body.message);
-
-  //   return body;
-  // };
 
   render() {
     return (
       <div>
         <NavBar />
         <JokeBook />
-        <div className="App-intro">{this.state.response.map((joke) => {return <div key={joke.id}>Name: {joke.name} Content: {joke.content} </div> })}</div>
       </div>
     );
   }
