ad159c3fe1e949be9e655a4397910e7e4bb37a0a
Elyott Ryan <elyott_ryan@hotmail.com>
2018-10-10 19:02:57 -0700

    Added action fetchJokes

--- client/src/JokeList.js
+++ client/src/JokeList.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import JokeItem from './JokeItem';
+import { fetchJokes } from './actions/index';
+import { bindActionCreators } from 'redux';
 
 class JokeList extends Component {
+    constructor() {
+    super();
+
+    this.state = { response: [] };
+  }
+
+    componentDidMount() {
+
+      fetch("http://localhost:5000/api/jokes")
+        .then(results => {
+          console.log(results);
+          return results.json();
+        })
+        .then(data => {
+          console.log(data);
+          this.setState({
+            response: data
+          });
+        });
+      // this.callApi()
+      //   .then(res => this.setState({ response: res.express }))
+      //   .catch(err => console.log(err));
+    }
+
   renderJokeList() {
     return this.props.jokes.map((joke) => {
       return (<JokeItem
@@ -24,4 +50,11 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(JokeList);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({
+      fetchJokes
+    },
+    dispatch
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(JokeList);
