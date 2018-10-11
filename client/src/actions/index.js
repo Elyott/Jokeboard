8bd7d9e42eb7e8c974c89ff43af18208a028689b
Elyott Ryan <elyott_ryan@hotmail.com>
2018-10-10 19:03:16 -0700

    created fetchJokes action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,29 @@
+export const JOKE_SELECTED = 'JOKE_SELECTED';
+export const FETCH_JOKES = 'FETCH_JOKES';
+
 export function selectJoke(joke) {
-  return{
-    type: 'JOKE_SELECTED',
+  return {
+    type: JOKE_SELECTED,
     payload: joke
+  }
+}
+
+export function fetchJokes(){
+
+  const request = (fetch("http://localhost:5000/api/jokes")
+    .then(results => {
+      console.log(results);
+      return results.json();
+    })
+    .then(data => {
+      console.log(data);
+      this.setState({
+        response: data
+      });
+    })
+  );
+  return {
+    type: FETCH_JOKES,
+    payload: request
   }
 }
