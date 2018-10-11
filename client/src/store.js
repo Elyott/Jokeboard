c49e77f285ec19c86c7c64e225f30c017488c0f9
Elyott Ryan <elyott_ryan@hotmail.com>
2018-10-11 16:17:21 -0700

    added redux-promise middleware

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,10 @@
-import {
-  createStore,
-  applyMiddleware
-} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise';
 import rootReducer from './reducers/rootReducer';
 export default function configureStore() {
   return createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, promise)
   );
 }
