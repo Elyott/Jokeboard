6685cc5c8377b3c200fd0f441fedd09b03445fa3
Elyott Ryan <elyott_ryan@hotmail.com>
2018-10-13 15:54:38 -0700

    updated server

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 const routes = require('./routes');
 
-const { Pool } = require('pg');
-const pool = new Pool({
+const { Client } = require('pg');
+
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: true,
 });
 
-pool.connect();
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
