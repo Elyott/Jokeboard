80c50b777a7b628c90dcf6ef599d58e7ffd3bdca
Elyott Ryan <elyott_ryan@hotmail.com>
2018-10-24 15:55:31 -0700

    added form reducer to root reducers

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import JokesReducer from './reducer_jokes';
 import ActiveJoke from './reducer_active_joke';
 
 export default combineReducers({
+  form: formReducer,
   jokes: JokesReducer,
   activeJoke: ActiveJoke
 });
