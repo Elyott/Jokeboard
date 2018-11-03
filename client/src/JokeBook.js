ba7535ea6fd66179c5adb460040c9975641430c2
Elyott Ryan <elyott_ryan@hotmail.com>
2018-11-03 12:52:26 -0700

    Changed new joke to a link and passed history to jokedetail

--- client/src/JokeBook.js
+++ client/src/JokeBook.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import JokeList from './JokeList';
 import JokeDetail from './JokeDetail';
+import { Link } from 'react-router-dom';
 
 class JokeBook extends Component {
 
@@ -10,9 +11,9 @@ class JokeBook extends Component {
         <div className='title'>Joke Book</div>
         <div className='container' >
             <JokeList />
-            <JokeDetail />
+            <JokeDetail history={this.props.history}/>
         </div>
-        <button className='add_joke_btn'>Add New Joke</button>
+        <Link to="/newjoke" className="add_joke_btn">Add New Joke</Link>
       </div>
     );
   }
