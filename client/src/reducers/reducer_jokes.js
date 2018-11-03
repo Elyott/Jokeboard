b46776705943f84688c45ebd58f8ee8beab5fbb8
Elyott Ryan <elyott_ryan@hotmail.com>
2018-11-03 14:09:06 -0700

    Added Delete_Joke to joke reducer

--- client/src/reducers/reducer_jokes.js
+++ client/src/reducers/reducer_jokes.js
@@ -1,8 +1,10 @@
-import { FETCH_JOKES } from '../actions/index';
+import { FETCH_JOKES, DELETE_JOKE } from '../actions/index';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case DELETE_JOKE:
+      return _.omit(state, action.payload);
     case FETCH_JOKES:
       return _.mapKeys(action.payload.data, 'id');
     default:
