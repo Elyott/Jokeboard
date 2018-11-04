492e725f24c67a532ebd107fda5d3af9b7391f42
Elyott Ryan <elyott_ryan@hotmail.com>
2018-11-04 10:27:09 -0800

    Moved onSubmit to NewJoke component and sent it as a prop

--- client/src/NewJoke.js
+++ client/src/NewJoke.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import JokeForm from './JokeForm';
+import { connect } from 'react-redux';
+import { createJoke, selectJoke } from './actions';
 
 class NewJoke extends Component {
+
+  onSubmit = (values) => {
+    values["user_id"] = 2;
+    this.props.createJoke(values, (joke) => {
+      this.props.selectJoke(joke);
+      this.props.history.push('/jokes');
+    });
+  }
 
   render(){
     return (
       <div className="newjoke_container">
         <div className="form-group">
           <div className="newjoke_title">Create A New Joke</div>
-          <JokeForm history={this.props.history}/>
+          <JokeForm onSubmit={this.onSubmit}/>
         </div>
       </div>
     );
   }
 }
 
-export default NewJoke;
+export default connect(null,{ createJoke, selectJoke })(NewJoke);
