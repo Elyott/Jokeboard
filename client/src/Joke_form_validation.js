b1be85fb0df77fc369ffdb428bafd6ad6de6ab77
Elyott Ryan <elyott_ryan@hotmail.com>
2018-11-04 10:40:57 -0800

    Moved Joke form validation to it's own file

---
+++ client/src/Joke_form_validation.js
@@ -0,0 +1,42 @@
+export function validate(values) {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = "Enter a Name!";
+  }
+
+  if (!values.rating) {
+    errors.rating = "Enter a Rating!";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter a Joke!";
+  }
+
+  if (values.seconds < "0" || values.seconds > "60") {
+    errors.seconds = "Must be a number between 0 and 60!";
+  }
+
+  if (isNaN(values.minutes)) {
+    errors.minutes = "Please Enter a Number!";
+  }
+
+  if (isNaN(values.seconds)) {
+    errors.seconds = "Please Enter a Number!";
+  }
+
+  if (!values.minutes && values.seconds) {
+    errors.minutes = "";
+  }
+
+  if (values.minutes && !values.seconds) {
+    errors.seconds = "";
+  }
+
+  if ((!values.minutes && !values.seconds) || (!values.minutes && values.seconds === "0")) {
+    errors.seconds = "Enter a Duration!";
+    errors.minutes = "";
+  }
+
+  return errors;
+}
