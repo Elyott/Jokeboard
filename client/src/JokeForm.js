54bfecf9f88f4d73c8bac28cd07aefde09e70c5a
Elyott Ryan <elyott_ryan@hotmail.com>
2018-11-04 10:41:30 -0800

    Moved validation to own file then imported it

--- client/src/JokeForm.js
+++ client/src/JokeForm.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 
+import { validate } from './Joke_form_validation';
+
 const ratingOptions = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "F"]
 
 class JokeForm extends Component {
@@ -114,48 +116,7 @@ class JokeForm extends Component {
   }
 }
 
-function validate(values) {
-  const errors = {};
 
-  if (!values.name) {
-    errors.name = "Enter a Name!";
-  }
-
-  if (!values.rating) {
-    errors.rating = "Enter a Rating!";
-  }
-
-  if (!values.content) {
-    errors.content = "Enter a Joke!";
-  }
-
-  if (values.seconds < "0" || values.seconds > "60") {
-    errors.seconds = "Must be a number between 0 and 60!";
-  }
-
-  if (isNaN(values.minutes)) {
-    errors.minutes = "Please Enter a Number!";
-  }
-
-  if (isNaN(values.seconds)) {
-    errors.seconds = "Please Enter a Number!";
-  }
-
-  if (!values.minutes && values.seconds) {
-    errors.minutes = "";
-  }
-
-  if (values.minutes && !values.seconds) {
-    errors.seconds = "";
-  }
-
-  if ((!values.minutes && !values.seconds) || (!values.minutes && values.seconds === "0")) {
-    errors.seconds = "Enter a Duration!";
-    errors.minutes = "";
-  }
-
-  return errors;
-}
 
 export default reduxForm({
   form: 'NewJokeForm',
