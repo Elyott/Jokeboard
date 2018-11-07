2d3bd9e2c97d8ac5a3b95dae8b40a913de8533a0
Elyott Ryan <elyott_ryan@hotmail.com>
2018-11-07 08:50:45 -0800

    Moved login form to its own component User form

--- client/src/UserForm.js
+++ client/src/UserForm.js
@@ -10,9 +10,9 @@ class UserForm extends Component {
     type,
     meta: { touched, error, warning }
   }) => (
-      <div className={`form-group row`}>
-        <label className="col-sm-3 col-form-label">{label}</label>
-        <div className="col-sm-9">
+      <div className={`form-group`}>
+        <label className="">{label}</label>
+        <div className="">
           <input
             {...input}
             className="form-control"
@@ -24,18 +24,15 @@ class UserForm extends Component {
       </div>
   );
 
-  onSubmit = (values) => {
-    console.log(values);
-  }
 
   render() {
-    const { handleSubmit, submitting } = this.props;
+    const { handleSubmit, submitting, onSubmit, title, message, linkButton, linkRoute} = this.props;
 
     return (
       <div className="login_container">
         <div className="form-group">
-          <div className="login_title">Login</div>
-          <form onSubmit={handleSubmit(this.onSubmit)}>
+          <div className="login_title">{title}</div>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <Field
               name="email"
               type="email"
@@ -48,13 +45,21 @@ class UserForm extends Component {
               component={this.renderField}
               label="Password"
             />
+            {title === 'Sign Up' &&
+            <Field
+              name="confirmpassword"
+              type="password"
+              component={this.renderField}
+              label="Confirm Password"
+            />
+            }
             <button type="submit" disabled={submitting} className="btn btn-primary">
-              Login
+              {title}
             </button>
           </form>
         </div>
-        <div className="signup_link">New to Jokebook?
-          <Link to="/signup" className="signup_link_button"> Create an account</Link>
+        <div className="signup_link">{message}
+            <Link to={linkRoute} className="signup_link_button">{linkButton}</Link>
         </div>
       </div>
     );
@@ -65,10 +70,25 @@ const validate = values => {
   const errors = {}
 
   if (!values.email) {
-    errors.email = 'Required'
+    errors.email = 'Please enter Email'
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address'
+    errors.email = 'Please enter a valid Email'
   }
+
+  if (!values.password) {
+    errors.password = 'Please enter Password'
+  } else if (values.password.length < 6) {
+    errors.password = 'Password must be at least 6 characters'
+  }
+
+  if (!values.confirmpassword) {
+    errors.confirmpassword = 'Please enter Password again'
+  } else if (values.confirmpassword.length < 6) {
+    errors.confirmpassword = 'Password must be at least 6 characters'
+  } else if (values.confirmpassword !== values.password) {
+    errors.confirmpassword = 'Passwords do not match'
+  }
+
 
 
   return errors
