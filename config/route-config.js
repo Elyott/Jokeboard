ba0136b5ad0986de129686d7db0aa0aac7552dfb
Elyott Ryan <elyott_ryan@hotmail.com>
2018-11-10 15:39:18 -0800

    Moved routes to route-config file added auth routes

---
+++ config/route-config.js
@@ -0,0 +1,18 @@
+(function (routeConfig) {
+
+  'use strict';
+
+  routeConfig.init = function (app) {
+
+    // *** routes *** //
+    const routes = require('../routes/index');
+    const authRoutes = require('../routes/auth');
+
+
+    // *** register routes *** //
+    app.use('/api', routes);
+    app.use('/auth', authRoutes);
+
+  };
+
+})(module.exports);
