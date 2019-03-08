616cae58887153a2d9e77f7bbd1cb55dc55c8733
Elyott Ryan <elyott_ryan@hotmail.com>
2019-03-07 20:54:26 -0800

    Updated SetlistItem changed class name and props to setlist

--- client/src/SetlistItem.js
+++ client/src/SetlistItem.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { selectJoke } from './actions/index';
 
- class JokeItem extends Component {
+ class SetlistItem extends Component {
   render() {
     return(
-      <div className='joke' onClick={() => this.props.selectJoke(this.props.joke)}>
-        <div className='joke_name'>{this.props.joke.name}</div>
+      // <div className='joke' onClick={() => this.props.selectJoke(this.props.joke)}>
+      //   <div className='joke_name'>{this.props.setlist.name}</div>
+      // </div>
+      <div>
+      <div className='joke'>
+        <div className='joke_name'>{this.props.setlist.name}
+        </div>
+      </div>
+      <button className="btn">
+        X
+      </button>
       </div>
     );
   }
 }
 
 
-export default connect(null, {selectJoke})(JokeItem);
+export default connect(null, {selectJoke})(SetlistItem);
