1a428b37e905e2fb2e8a3f9582fc9631775492d8
Elyott Ryan <elyott_ryan@hotmail.com>
2019-03-07 19:54:15 -0800

    Added setlist queries

--- db/queries.js
+++ db/queries.js
@@ -4,13 +4,18 @@ function Jokes() {
   return knex('jokes');
 }
 
-// *** queries *** //
+function Setlists() {
+  return knex('setlists');
+}
 
-function getAll() {
+
+// *** joke queries *** //
+
+function getAllJokes() {
   return Jokes().select();
 }
 
-function getSingle(jokeID) {
+function getSingleJoke(jokeID) {
   return Jokes().where('id', parseInt(jokeID)).first();
 }
 
@@ -26,15 +31,44 @@ function deleteJoke(jokeID) {
   return Jokes().where('id', parseInt(jokeID)).del();
 }
 
+// *** setlist queries *** //
+
+function getAllSetlists() {
+  return Setlists().select();
+}
+
+function getSingleSetlist(setlistID) {
+  return Setlists().where('id', parseInt(setlistID)).first();
+}
+
+function addSetlist(joke) {
+  return Setlists().insert(setlist, 'id');
+}
+
+function updateSetlist(setlistID, updates) {
+  return Setlists().where('id', parseInt(setlistID)).update(updates);
+}
+
+function deleteSetlist(setlistID) {
+  return Setlists().where('id', parseInt(setlistID)).del();
+}
+
+// *** users queries ***
+
 function getAllUsers() {
   return knex('users').select();
 }
 
 module.exports = {
-  getAll,
-  getSingle,
+  getAllJokes,
+  getSingleJoke,
   addJoke,
   updateJoke,
   deleteJoke,
+  getAllSetlists,
+  getSingleSetlist,
+  addSetlist,
+  updateSetlist,
+  deleteSetlist,
   getAllUsers
 };
