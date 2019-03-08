4cc0356fff558821bc91fe5270c4b9dac4a4bd9e
Elyott Ryan <elyott_ryan@hotmail.com>
2019-03-07 20:55:37 -0800

    Added active setlist to redux state

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -3,10 +3,12 @@ import { reducer as formReducer } from 'redux-form';
 import JokesReducer from './reducer_jokes';
 import SetlistsReducer from './reducer_setlists';
 import ActiveJoke from './reducer_active_joke';
+import ActiveSetlist from './reducer_active_setlist';
 
 export default combineReducers({
   form: formReducer,
   jokes: JokesReducer,
   setlists: SetlistsReducer,
-  activeJoke: ActiveJoke
+  activeJoke: ActiveJoke,
+  activeSetlist: ActiveSetlist
 });
