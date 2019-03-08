80b6dd16afea8d8cd5617c2b6bd0bf609202d642
Elyott Ryan <elyott_ryan@hotmail.com>
2019-03-07 21:12:57 -0800

    removed selectJoke div

--- client/src/SetlistItem.js
+++ client/src/SetlistItem.js
@@ -5,9 +5,6 @@ import { selectSetlist } from './actions/index';
  class SetlistItem extends Component {
   render() {
     return(
-      // <div className='joke' onClick={() => this.props.selectJoke(this.props.joke)}>
-      //   <div className='joke_name'>{this.props.setlist.name}</div>
-      // </div>
       <div>
       <div className='joke' onClick={() => this.props.selectSetlist(this.props.setlist)}>
         <div className='joke_name'>{this.props.setlist.name}
