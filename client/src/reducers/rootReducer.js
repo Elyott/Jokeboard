0516f534dda2352b1ad3d92e9f92e1488ad5fc2f
Elyott Ryan <elyott_ryan@hotmail.com>
2019-03-07 20:39:43 -0800

    Added setlists to state

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import JokesReducer from './reducer_jokes';
+import SetlistsReducer from './reducer_setlists';
 import ActiveJoke from './reducer_active_joke';
 
 export default combineReducers({
   form: formReducer,
   jokes: JokesReducer,
+  setlists: SetlistsReducer,
   activeJoke: ActiveJoke
 });
